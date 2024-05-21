f940f9e301f1dd4a779f217c21a0fc2539602be0
Kaitru <125087336+Kaitru@users.noreply.github.com>
2024-05-21 18:09:11 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+homeworks = 12
+hours = 1.5
+course = 'Python'
+hours_per_work = hours / homeworks
+
+print(f'Курс: {course}, всего задач: {homeworks}, затраченное время: {hours}, среднее время: {hours_per_work}')
