37894c3871c3d2bb1a0528ef498f177af7c42b55
Kaitru <125087336+Kaitru@users.noreply.github.com>
2024-05-24 20:20:21 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,11 @@
+my_list = ['apple', 'banana', 'fish', 'grape', 'Python', 'cat']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'FISH'
+print(my_list)
+
+my_dict = {'cat': 'кошка', 'dog': 'собака'}
+print(my_dict)
+print(my_dict.setdefault('cat'))
