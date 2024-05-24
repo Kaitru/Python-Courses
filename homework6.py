c4163c99eac325e0cc968904a26224ee6db8ca04
Kaitru <125087336+Kaitru@users.noreply.github.com>
2024-05-24 20:43:24 +0300

    Add files via upload

--- homework6.py
+++ homework6.py
@@ -1,13 +1,28 @@
-my_list = ['apple', 'banana', 'fish', 'grape', 'Python', 'cat']
-print(my_list)
-print(my_list[0])
-print(my_list[-1])
-print(my_list[2:5])
-my_list[2] = 'FISH'
-print(my_list)
+my_dict = {"Кирилл": 2004,
+           "Кристина": 2002,
+           "Алексей": 2007
+           }
+print(my_dict)
 
-my_dict = {'cat': 'кошка', 'dog': 'собака'}
+print(my_dict.get("Кирилл"))
+print(my_dict.get("Антон"))
+
+my_dict["Семён"] = 2006
+my_dict["Вася"] = 1997
+
+deleted_value = my_dict.pop("Алексей")
+print("Удаленное значение:", deleted_value)
+
 print(my_dict)
-print(my_dict.setdefault('cat'))
-my_dict['fish'] = 'рыба'
-print(my_dict)
+
+
+my_set = {1, "hello", 3.14, True, 1, "hello", None, (1, 2, 3)}
+
+print("Исходное множество:", my_set)
+
+my_set.add("new_string")
+my_set.add(42)
+
+my_set.remove(3.14)
+
+print("Измененное множество:", my_set)
