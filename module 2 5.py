afb4447004bac8905f3a8ebd537b8404e27be44e
Kaitru <125087336+Kaitru@users.noreply.github.com>
2024-05-27 20:27:57 +0300

    Add files via upload

---
+++ module 2 5.py
@@ -0,0 +1,12 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for _ in range(n):
+        matrix.append([value for _ in range(m)])
+    return matrix
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
