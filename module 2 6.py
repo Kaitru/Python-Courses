0704787253386bc218eb0d9455a52694d8eb6b80
Kaitru <125087336+Kaitru@users.noreply.github.com>
2024-06-04 15:25:42 +0300

    Add files via upload

---
+++ module 2 6.py
@@ -0,0 +1,12 @@
+def single_root_words(root_word, *other_words):
+    result = []
+    for i in range(len(other_words)):
+        if root_word.lower() in other_words[i].lower() or other_words[i].lower() in root_word.lower():
+            result.append(other_words[i])
+    return result
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
