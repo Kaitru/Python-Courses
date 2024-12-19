96d56176135f7df1331c3c7d2ab4c390320be14e
Kaitru <125087336+Kaitru@users.noreply.github.com>
2024-12-19 20:09:24 +0300

    Add files via upload

---
+++ module_16_1.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Главная страница"}
+
+@app.get("/user/admin")
+async def admin():
+    return {"message": "Вы вошли как администратор"}
+
+@app.get("/user/{user_id}")
+async def user_id(user_id: int):
+    return {"message": f"Вы вошли как пользователь №{user_id}"}
+
+@app.get("/user")
+async def user_page(username: str, age: int):
+    return {"message": f"Информация о пользователе. Имя: {username}. Возраст: {age}."}
